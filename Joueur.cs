25e5c23b93f096b80e5d46dedae3ade85e23016c
Arthur Savournin <91455639+arthursavournin@users.noreply.github.com>
2024-11-07 08:34:33 +0100

    Add files via upload

---
+++ Joueur.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Algo
+{
+    internal class Joueur
+    {
+        private string nom;
+        private int score;
+        private List<string> mots;
+
+
+        public Joueur (string nom)
+        {
+            this.nom = nom;
+            this.score = 0;
+            this.mots = new List<string> ();
+        }
+
+
+
+        public int Score
+        { 
+            get { return score; }
+            set { score = value; }
+        }
+
+        public string Nom
+        { 
+            get { return nom; } 
+        }
+
+        public List<string> Mots
+        { 
+            get { return mots; } 
+        }
+
+
+
+        public bool Contain(string mot)
+        {
+            return (mots.Contains (mot));
+        }
+
+        public void AddMot(string mot)
+        {
+            mots.Add (mot);
+        }
+
+
+        public string toString()
+        {
+            string texte = "Nom : " + nom + "\nScore : " + score + "\nMots trouvés : \n";
+            for (int i = 0; i < mots.Count; i++)
+            {
+                texte += mots[i]+"\n";
+            }
+            return texte;
+        }
+    }
+}
