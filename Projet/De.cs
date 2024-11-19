056e08ca80315b32175c0dac525a37495e35de45
Arthur Savournin <91455639+arthursavournin@users.noreply.github.com>
2024-11-20 00:42:13 +0100

    Update De.cs

--- Projet/De.cs
+++ Projet/De.cs
@@ -8,47 +8,52 @@ using System.Threading.Tasks;
 namespace Projet_Algo
 {
     internal class De
+{
+    private Lettre[] faces;
+    private Random random;
+    public De()
     {
-        private char[] faces;
-        private Random random;
-        public De()
+        this.faces = new Lettre[6];
+        this.random = new Random();
+    }
+    /// <summary>
+    /// Cette méthode retourne une chaîne de caractère qui décrit les faces d'un dé
+    /// </summary>
+    /// <returns>s</returns>
+    public string toString()
+    {
+        string s = "";
+        for (int i = 0; i < this.faces.Length; i++)
         {
-            this.faces = new char[6];
-            this.random = new Random();
+            s = s + this.faces[i].Valeur + " ";
         }
-        /// <summary>
-        /// Cette méthode retourne une chaîne de caractère qui décrit les faces d'un dé
-        /// </summary>
-        /// <returns>s</returns>
-        public string toString()
-        {
-            string s = "";
-            for (int i = 0; i < this.faces.Length; i++)
-            {
-                s = s + this.faces[i] + " ";
-            }
-            return s;
-        }
-        /// <summary>
-        /// Cette méthode permet de tirer au hasard une lettre parmi les 6.
-        /// </summary>
-        /// <param name="r"></param>
-        public void Lance()
-        {
-            Console.Write(this.faces[random.Next(this.faces.Length)]);
-        }
-        /// <summary>
-        /// Cette méthode permet de remplir les faces du dé en sélectionnant 6 lettres en fonction de leur fréquences de façon aléatoire
-        /// </summary>
-        public void DefinirLettres()
-        {
-            char[] LettresDispo = Lettre.ObtenirLettre();
+        return s;
+    }
+    
+    /// <summary>
+    /// Cette méthode permet de remplir les faces du dé en sélectionnant 6 lettres en fonction de leur fréquences de façon aléatoire
+    /// </summary>
+    public void DefinirLettres()
+    {
+        List<Lettre> LettresDispo = Lettre.ObtenirLettre();
+        List<Lettre> LettresDispoTemp = new List<Lettre>();
+        LettresDispoTemp.AddRange(LettresDispo);
 
-            for (int i = 0; i < 6; i++)
-            {
-                int choix = random.Next(LettresDispo.Length);
-                faces[i] = LettresDispo[choix];
-            }
+        for (int i = 0; i < 6; i++)
+        {
+            int choix = random.Next(LettresDispoTemp.Count());
+            faces[i] = LettresDispoTemp[choix];
+            LettresDispoTemp.Remove(LettresDispoTemp[choix]);
         }
     }
+
+    /// <summary>
+    /// Cette méthode permet de tirer au hasard une lettre parmi les 6.
+    /// </summary>
+    /// <param name="r"></param>
+    public Lettre Lance()
+    {
+        return this.faces[random.Next(this.faces.Length)];
+    }
+}
 }
