c6eb50a580509a0aa33bc5ba06227062e2768758
Arthur Savournin <91455639+arthursavournin@users.noreply.github.com>
2024-11-20 00:49:10 +0100

    Add files via upload

--- Projet/Lettre.cs
+++ Projet/Lettre.cs
@@ -5,93 +5,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Projet_Algo
+namespace Projet
 {
     internal class Lettre
-{
-    public char valeur;
-    public int nombre;
-    public int poids;
-    public char Valeur
     {
-        get { return valeur; }
-        set { valeur = value; }
-    }
-    public int Nombre
-    {
-        get { return nombre; }
-        set { nombre = value; }
-    }
-    public int Poids
-    {
-        get { return poids; }
-        set { poids = value; }
-    }
-    public Lettre(char valeur, int poids, int nombre)
-    {
-        this.valeur = valeur;
-        this.poids = poids;
-        this.nombre = nombre;
-    }
-
-    /// <summary>
-    /// Méthode qui crée un tableau de Lettres à partir du fichier texte
-    /// </summary>
-    /// <returns></returns>
-    public static Lettre[] LettresDispo()
-    {
-        Lettre[] lettres = new Lettre[26];
-        string[] lignes = File.ReadAllLines("Lettres.txt");
-        for (int i= 0; i < lignes.Length; i++)
+        public char valeur;
+        public int poids;
+        public int nombre;
+        public char Valeur
         {
-            string[] ligne = lignes[i].Split(';');
-
-            char valeur = Convert.ToChar(ligne[0]);
-            int poids = Convert.ToInt32(ligne[1]);
-            int nombre = Convert.ToInt32(ligne[2]);
-
-            lettres[i] = new Lettre(valeur, nombre, poids);
+            get { return valeur; }
+            set { valeur = value; }
         }
-        return lettres;
-    }
-
-    /// <summary>
-    /// Méthode pour obtenir une liste de lettres avec leur fréquence
-    /// </summary>
-    /// <returns>lettres (un char[])</returns>
-    public static char[] ObtenirLettre()
-    {
-        int taille = 0;
-        Lettre[] lettresdispo = LettresDispo();
-        for (int i = 0; i < lettresdispo.Length; i++)
+        public int Nombre
         {
-            taille = taille + lettresdispo[i].nombre;
+            get { return nombre; }
+            set { nombre = value; }
         }
-        char[] lettres = new char[taille];
-        int n;
-        int c = 0;
-        for (int j = 0; j < lettresdispo.Length; j++)
+        public int Poids
         {
-            n = lettresdispo[j].nombre;
-            while (n > 0)
+            get { return poids; }
+            set { poids = value; }
+        }
+        public Lettre(char valeur, int poids, int nombre)
+        {
+            this.valeur = valeur;
+            this.poids = poids;
+            this.nombre = nombre;
+        }
+
+        /// <summary>
+        /// Méthode qui crée un tableau de Lettres à partir du fichier texte
+        /// </summary>
+        /// <returns></returns>
+        public static Lettre[] LettresDispo()
+        {
+            Lettre[] lettres = new Lettre[26];
+            string[] lignes = File.ReadAllLines("Lettres.txt");
+            for (int i= 0; i < lignes.Length; i++)
             {
-                lettres[c] = lettresdispo[j].valeur;
-                n--;
-                c++;
+                string[] ligne = lignes[i].Split(';');
+
+                char valeur = Convert.ToChar(ligne[0]);
+                int poids = Convert.ToInt32(ligne[1]);
+                int nombre = Convert.ToInt32(ligne[2]);
+
+                lettres[i] = new Lettre(valeur, poids, nombre);
+            }
+            return lettres;
+        }
+
+        /// <summary>
+        /// Méthode pour obtenir une liste de lettres avec leur fréquence
+        /// </summary>
+        /// <returns>lettres (un char[])</returns>
+        public static List<Lettre> ObtenirLettre()
+        {
+            Lettre[] lettresdispo = LettresDispo();
+            List<Lettre> lettres = new List<Lettre>();
+            for (int j = 0; j < lettresdispo.Length; j++)
+            {
+                int n = lettresdispo[j].nombre;
+                while (n > 0)
+                {
+                    lettres.Add(lettresdispo[j]);
+                    n--;
+                }
+            }
+            return lettres;
+        }
+        /// <summary>
+        /// Cette méthode permet d'afficher le tableau de caractère
+        /// </summary>
+        /// <param name="l"></param>
+        public static void Affichertab(Lettre[] l)//pour vérifier que le tableau retourner par ObtenirLettre est correct
+        {
+            for (int i = 0; i < l.Length; i++)
+            {
+                Console.Write(l[i].Valeur + " ");
             }
         }
-        return lettres;
     }
-    /// <summary>
-    /// Cette méthode permet d'afficher le tableau de caractère
-    /// </summary>
-    /// <param name="l"></param>
-    public static void Affichertab(char[] l)//pour vérifier que le tableau retourner par ObtenirLettre est correct
-    {
-        for (int i = 0; i < l.Length; i++)
-        {
-            Console.Write(l[i] + " ");
-        }
-    }
-}
 }
--- Projet/Program.cs
+++ Projet/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Projet_Algo
+namespace Projet
 {
     internal class Program
     {
@@ -14,12 +14,12 @@ namespace Projet_Algo
             De de = new De();
             de.DefinirLettres();
             Console.WriteLine(de.toString());
-            de.Lance();
+            Console.WriteLine(de.Lance().Valeur);
 
-            Dictionnaire b = new Dictionnaire("test");
-            Console.WriteLine(b.Dico());
-            Console.WriteLine(b.toString(8, 'a'));
-            Console.WriteLine(b.RechDicoRecursif("tuer"));
+            //Dictionnaire b = new Dictionnaire("test");
+            //Console.WriteLine(b.Dico());
+            //Console.WriteLine(b.toString(8, 'a'));
+            //Console.WriteLine(b.RechDicoRecursif("tuer"));
         }
     }
 }
