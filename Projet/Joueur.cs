1fe34090086d998486c22aa5c094fa541bdb9b71
Arthur Savournin <91455639+arthursavournin@users.noreply.github.com>
2024-11-20 17:26:51 +0100

    Add files via upload

---
+++ Projet/Joueur.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    internal class Joueur
+    {
+        private string nom;
+        private int score;
+        private List<string> mots;
+
+
+        public Joueur(string nom)
+        {
+            this.nom = nom;
+            this.score = 0;
+            this.mots = new List<string>();
+        }
+
+
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+        }
+
+        public List<string> Mots
+        {
+            get { return mots; }
+        }
+
+
+
+        public bool Contain(string mot)
+        {
+            return (mots.Contains(mot));
+        }
+
+        /// <summary>
+        /// Ajouter le mot à la liste des mots déja trouvés par le joueur
+        /// </summary>
+        /// <param name="mot">Mot trouvé par le joueur</param>
+
+        public void AddMot(string mot)
+        {
+            mots.Add(mot);
+        }
+
+
+        public string toString()
+        {
+            string texte = "Nom : " + nom + "\nScore : " + score + "\nMots trouvés : \n";
+            for (int i = 0; i < mots.Count; i++)
+            {
+                texte += mots[i] + "\n";
+            }
+            return texte;
+        }
+    }
+}
---
+++ Projet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            De de = new De();
+            de.DefinirLettres();
+            Console.WriteLine(de.toString());
+            Console.WriteLine(de.Lance().Valeur);
+
+            //Dictionnaire b = new Dictionnaire("test");
+            //Console.WriteLine(b.Dico());
+            //Console.WriteLine(b.toString(8, 'a'));
+            //Console.WriteLine(b.RechDicoRecursif("tuer"));
+        }
+    }
+}
