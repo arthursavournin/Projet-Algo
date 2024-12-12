57271b95fa0af751c6e7432fce7de28417497c41
Sanchez8888 <clemsanch@gmail.com>
2024-12-12 11:58:18 +0100

    Update Joueur.cs

--- Projet/Joueur.cs
+++ Projet/Joueur.cs
@@ -8,67 +8,67 @@ using System.Threading.Tasks;
 namespace Projet
 {
     internal class Joueur
+{
+    private string nom;
+    private int score;
+    private List<string> mots;
+
+
+    public Joueur(string nom)
     {
-        private string prenom;
-        private int points;
-        private List<string> mots_trouvés;
-
-
-        public Joueur(string nom)
-        {
-            this.prenom = nom;
-            this.points = 0;
-            this.mots_trouvés = new List<string>();
-        }
-
-
-
-        public int Points
-        {
-            get { return points; }
-            set { points = value; }
-        }
-
-        public string Prenom
-        {
-            get { return prenom; }
-        }
-
-        public List<string> Mots_trouvés
-        {
-            get { return mots_trouvés; }
-        }
-
-
-        /// <summary>
-        /// Méthode qui vérifie si le joueur à déja trouvé le mot 
-        /// </summary>
-        /// <param name="mot">Mot que le joueur vient de trouvé</param>
-        /// <returns>Vrai s'il a déjà trouvé ce mot ou faux sinon</returns>
-        public bool Contain(string mot)
-        {
-            return (mots_trouvés.Contains(mot));
-        }
-
-        /// <summary>
-        /// Ajouter le mot à la liste des mots déja trouvés par le joueur
-        /// </summary>
-        /// <param name="mot">Mot trouvé par le joueur</param>
-
-        public void AddMot(string mot)
-        {
-            mots_trouvés.Add(mot);
-        }
-
-
-        public string toString()
-        {
-            string texte = "Nom : " + prenom + "\nScore : " + points + "\nMots trouvés : \n";
-            for (int i = 0; i < mots.Count; i++)
-            {
-                texte += mots_trouvés[i] + "\n";
-            }
-            return texte;
-        }
+        this.nom = nom;
+        this.score = 0;
+        this.mots = new List<string>();
     }
+
+
+
+    public int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    public string Nom
+    {
+        get { return nom; }
+    }
+
+    public List<string> Mots
+    {
+        get { return mots; }
+    }
+
+
+    /// <summary>
+    /// Méthode qui vérifie si le joueur à déja trouvé le mot 
+    /// </summary>
+    /// <param name="mot">Mot que le joueur vient de trouvé</param>
+    /// <returns>Vrai s'il a déjà trouvé ce mot ou faux sinon</returns>
+    public bool Contain(string mot)
+    {
+        return (mots.Contains(mot));
+    }
+
+    /// <summary>
+    /// Ajouter le mot à la liste des mots déja trouvés par le joueur
+    /// </summary>
+    /// <param name="mot">Mot trouvé par le joueur</param>
+
+    public void AddMot(string mot)
+    {
+        mots.Add(mot);
+    }
+
+
+    public string toString()
+    {
+        string texte = "Nom : " + nom + "\nScore : " + score + "\nMots trouvés : \n";
+        for (int i = 0; i < mots.Count; i++)
+        {
+            texte += mots[i] + "\n";
+        }
+        return texte;
+    }
+}
 }
