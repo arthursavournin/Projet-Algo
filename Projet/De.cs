9cd19e0fbbb17bbac049b7c68c9d24df223afead
Sanchez8888 <clemsanch@gmail.com>
2024-12-12 12:06:20 +0100

    Update De.cs

--- Projet/De.cs
+++ Projet/De.cs
@@ -8,69 +8,69 @@ using System.Threading.Tasks;
 namespace Projet
 {
     internal class De
+{
+    private Lettre[] faces;
+    private Lettre faceVisible;
+
+    public De()
     {
-        private Lettre[] faces;
-        private Lettre faceVisible;
+        this.faces = new Lettre[6];
+        this.faceVisible = this.faces[0];
+    }
 
-        public De()
+    public Lettre[] Faces
+    {
+        get { return this.faces; }
+        set { faces = value; }
+    }
+
+    public Lettre FaceVisible
+    {
+        get { return this.faceVisible; }
+        set { this.faceVisible = value; }
+    }
+
+    /// <summary>
+    /// Cette méthode retourne une chaîne de caractère qui décrit les faces d'un dé
+    /// </summary>
+    /// <returns>s</returns>
+    public string toString()
+    {
+        string s = "";
+        for (int i = 0; i < this.faces.Length; i++)
         {
-            this.faces = new Lettre[6];
-            this.faceVisible = this.faces[0];
+            s = s + this.faces[i].Valeur + " ";
         }
+        return s;
+    }
 
-        public Lettre[] Faces
+    /// <summary>
+    /// Cette méthode permet de remplir les faces du dé en sélectionnant 6 lettres en fonction de leur fréquences de façon aléatoire
+    /// </summary>
+    public void DefinirLettres()
+    {
+        Random random = new Random();
+        List<Lettre> LettresDispo = Lettre.ObtenirLettre();
+        List<Lettre> LettresDispoTemp = new List<Lettre>();
+        LettresDispoTemp.AddRange(LettresDispo);
+
+        for (int i = 0; i < 6; i++)
         {
-            get { return this.faces; }
-            set { faces = value; }
-        }
-
-        public Lettre FaceVisible
-        {
-            get { return this.faceVisible; }
-            set { this.faceVisible = value; }
-        }
-
-        /// <summary>
-        /// Cette méthode retourne une chaîne de caractère qui décrit les faces d'un dé
-        /// </summary>
-        /// <returns>s</returns>
-        public string toString()
-        {
-            string s = "";
-            for (int i = 0; i < this.faces.Length; i++)
-            {
-                s = s + this.faces[i].Valeur + " ";
-            }
-            return s;
-        }
-
-        /// <summary>
-        /// Cette méthode permet de remplir les faces du dé en sélectionnant 6 lettres en fonction de leur fréquences de façon aléatoire
-        /// </summary>
-        public void DefinirLettres()
-        {
-            Random random = new Random();
-            List<Lettre> LettresDispo = Lettre.ObtenirLettre();
-            List<Lettre> LettresDispoTemp = new List<Lettre>();
-            LettresDispoTemp.AddRange(LettresDispo);
-
-            for (int i = 0; i < 6; i++)
-            {
-                int choix = random.Next(LettresDispoTemp.Count());
-                faces[i] = LettresDispoTemp[choix];
-                LettresDispoTemp.Remove(LettresDispoTemp[choix]);
-            }
-        }
-
-        /// <summary>
-        /// Cette méthode permet de tirer au hasard une lettre parmi les 6.
-        /// </summary>
-        /// <param name="r"></param>
-        public Lettre Lance()
-        {
-            Random random = new Random();
-            faceVisible = this.faces[random.Next(this.faces.Length)];
-            return faceVisible;
+            int choix = random.Next(LettresDispoTemp.Count());
+            faces[i] = LettresDispoTemp[choix];
+            LettresDispoTemp.Remove(LettresDispoTemp[choix]);
         }
     }
+
+    /// <summary>
+    /// Cette méthode permet de tirer au hasard une lettre parmi les 6.
+    /// </summary>
+    /// <param name="r"></param>
+    public Lettre Lance()
+    {
+        Random random = new Random();
+        faceVisible = this.faces[random.Next(this.faces.Length)];
+        return faceVisible;
+    }
+}
 }
