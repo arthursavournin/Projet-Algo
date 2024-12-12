75db053fd69a4b68df8568973dcd38a3f8c1d9ec
Sanchez8888 <clemsanch@gmail.com>
2024-12-12 11:57:51 +0100

    Update Lettre.cs

--- Projet/Lettre.cs
+++ Projet/Lettre.cs
@@ -8,83 +8,83 @@ using System.Threading.Tasks;
 namespace Projet
 {
     internal class Lettre
+{
+    public char valeur;
+    public int poids;
+    public int nombre;
+
+    public char Valeur
     {
-        public char valeur;
-        public int poids;
-        public int nombre;
+        get { return valeur; }
+        set { valeur = value; }
+    }
+    public int Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+    public int Poids
+    {
+        get { return poids; }
+        set { poids = value; }
+    }
+    public Lettre(char valeur, int poids, int nombre)
+    {
+        this.valeur = valeur;
+        this.poids = poids;
+        this.nombre = nombre;
+    }
 
-        public char Valeur
+    /// <summary>
+    /// Méthode qui crée un tableau de Lettres à partir du fichier texte
+    /// </summary>
+    /// <returns></returns>
+    public static Lettre[] LettresDispo()
+    {
+        Lettre[] lettres = new Lettre[26];
+        string[] lignes = File.ReadAllLines("Lettres.txt");
+        for (int i = 0; i < lignes.Length; i++)
         {
-            get { return valeur; }
-            set { valeur = value; }
-        }
-        public int Nombre
-        {
-            get { return nombre; }
-            set { nombre = value; }
-        }
-        public int Poids
-        {
-            get { return poids; }
-            set { poids = value; }
-        }
-        public Lettre(char valeur, int poids, int nombre)
-        {
-            this.valeur = valeur;
-            this.poids = poids;
-            this.nombre = nombre;
-        }
+            string[] ligne = lignes[i].Split(';');
 
-        /// <summary>
-        /// Méthode qui crée un tableau de Lettres à partir du fichier texte
-        /// </summary>
-        /// <returns></returns>
-        public static Lettre[] LettresDispo()
+            char valeur = Convert.ToChar(ligne[0]);
+            int poids = Convert.ToInt32(ligne[1]);
+            int nombre = Convert.ToInt32(ligne[2]);
+
+            lettres[i] = new Lettre(valeur, poids, nombre);
+        }
+        return lettres;
+    }
+
+    /// <summary>
+    /// Méthode pour obtenir une liste de lettres avec leur fréquence
+    /// </summary>
+    /// <returns>lettres (un char[])</returns>
+    public static List<Lettre> ObtenirLettre()
+    {
+        Lettre[] lettresdispo = LettresDispo();
+        List<Lettre> lettres = new List<Lettre>();
+        for (int j = 0; j < lettresdispo.Length; j++)
         {
-            Lettre[] lettres = new Lettre[26];
-            string[] lignes = File.ReadAllLines("Lettres.txt");
-            for (int i = 0; i < lignes.Length; i++)
+            int n = lettresdispo[j].nombre;
+            while (n > 0)
             {
-                string[] ligne = lignes[i].Split(';');
-
-                char valeur = Convert.ToChar(ligne[0]);
-                int poids = Convert.ToInt32(ligne[1]);
-                int nombre = Convert.ToInt32(ligne[2]);
-
-                lettres[i] = new Lettre(valeur, poids, nombre);
+                lettres.Add(lettresdispo[j]);
+                n--;
             }
-            return lettres;
         }
-
-        /// <summary>
-        /// Méthode pour obtenir une liste de lettres avec leur fréquence
-        /// </summary>
-        /// <returns>lettres (un char[])</returns>
-        public static List<Lettre> ObtenirLettre()
+        return lettres;
+    }
+    /// <summary>
+    /// Cette méthode permet d'afficher le tableau de caractère
+    /// </summary>
+    /// <param name="l"></param>
+    public static void Affichertab(Lettre[] l)//pour vérifier que le tableau retourner par ObtenirLettre est correct
+    {
+        for (int i = 0; i < l.Length; i++)
         {
-            Lettre[] lettresdispo = LettresDispo();
-            List<Lettre> lettres = new List<Lettre>();
-            for (int j = 0; j < lettresdispo.Length; j++)
-            {
-                int n = lettresdispo[j].nombre;
-                while (n > 0)
-                {
-                    lettres.Add(lettresdispo[j]);
-                    n--;
-                }
-            }
-            return lettres;
-        }
-        /// <summary>
-        /// Cette méthode permet d'afficher le tableau de caractère
-        /// </summary>
-        /// <param name="l"></param>
-        public static void Affichertab(Lettre[] l)//pour vérifier que le tableau retourner par ObtenirLettre est correct
-        {
-            for (int i = 0; i < l.Length; i++)
-            {
-                Console.Write(l[i].Valeur + " ");
-            }
+            Console.Write(l[i].Valeur + " ");
         }
     }
+}
 }
