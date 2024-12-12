26d5861d9ffcfd0c5ee80b095f7614bb1af1d5a3
Sanchez8888 <clemsanch@gmail.com>
2024-12-12 11:55:45 +0100

    Update Program.cs

--- Projet/Program.cs
+++ Projet/Program.cs
@@ -7,35 +7,36 @@ using System.Threading.Tasks;
 
 namespace Projet
 {
-    internal class Program
-    {
-        static string AfficherDesPlateau(Plateau plateau)
-        {
-            string texte = "";
-            for (int i = 0;i<plateau.Des.Count();i++)
-            {
-                texte += plateau.Des[i].toString() + "\n";
-            }
-            return texte;
-        }
-        static void Main(string[] args)
-        {
-            //De de = new De();
-            //de.DefinirLettres();
-            //Console.WriteLine(de.toString());
-            //Console.WriteLine(de.Lance().Valeur);
+     internal class Program
+ {
+     static string AfficherDesPlateau(Plateau plateau)
+     {
+         string texte = "";
+         for (int i = 0; i < plateau.Des.Count(); i++)
+         {
+             texte += plateau.Des[i].toString() + "\n";
+         }
+         return texte;
+     }
+     static void Main(string[] args)
+     {
+         //De de = new De();
+         //de.DefinirLettres();
+         //Console.WriteLine(de.toString());
+         //Console.WriteLine(de.Lance().Valeur);
 
-            //Dictionnaire b = new Dictionnaire("test");
-            //Console.WriteLine(b.Dico());
-            //Console.WriteLine(b.toString(8, 'a'));
-            //Console.WriteLine(b.RechDicoRecursif("tuer"));
+         //Dictionnaire b = new Dictionnaire("test");
+         //Console.WriteLine(b.Dico());
+         //Console.WriteLine(b.toString(8, 'a'));
+         //Console.WriteLine(b.RechDicoRecursif("tuer"));
 
-            Plateau plateau = new Plateau(4, 4, "français");
-            plateau.CreerPlateau();
-            Console.WriteLine(AfficherDesPlateau(plateau));
-            Console.WriteLine(plateau.AfficherPlateau());
-            //string mot = Console.ReadLine();
-            //Console.WriteLine(plateau.Test_Plateau(mot));
-        }
-    }
+         Plateau plateau = new Plateau(4, 4, "français");
+         plateau.CreerPlateau();
+         Console.WriteLine(AfficherDesPlateau(plateau));
+         Console.WriteLine(plateau.AfficherPlateau());
+         string mot = Console.ReadLine();
+         Console.WriteLine(plateau.Test_Plateau(mot));
+         Console.ReadKey();
+     }
+ }
 }
