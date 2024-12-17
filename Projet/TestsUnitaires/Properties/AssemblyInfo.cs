f93e530fb1e14d127b78d8a9793196eaaa84b2dc
Arthur Savournin <91455639+arthursavournin@users.noreply.github.com>
2024-12-17 15:03:08 +0100

    Add files via upload

---
+++ Projet/TestsUnitaires/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestsUnitaires")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestsUnitaires")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3db6bac0-c517-45de-b7fe-3c5d5b902beb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Projet/TestsUnitaires/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Projet
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRechDicoRecursif()
+        {
+            Dictionnaire dico = new Dictionnaire("français");
+            bool resultat = dico.RechDicoRecursif("maison");
+
+            Assert.AreEqual(true, resultat);
+        }
+
+        [TestMethod]
+        public void TestContain()
+        {
+            Joueur joueur = new Joueur("Test");
+            joueur.AddMot("maison");
+            bool resultat = joueur.Contain("maison");
+
+            Assert.AreEqual(true, resultat);
+
+        }
+
+        [TestMethod]
+        public void TestRechercheLettre()
+        {
+            Lettre a = new Lettre('A', 1, 9);
+            Lettre resultat = Lettre.RechercheLettre('a');
+
+            Assert.AreEqual(a.valeur, resultat.valeur);
+            Assert.AreEqual(a.poids, resultat.poids);
+            Assert.AreEqual(a.nombre, resultat.nombre);
+        }
+
+        [TestMethod]
+        public void TestTest_Plateau()
+        {
+            Random random = new Random();
+
+            Plateau plateau = new Plateau(16, 16, "français");
+            plateau.CreerPlateau(random);
+
+            bool resultat = plateau.Test_Plateau("le");
+
+            Assert.AreEqual(true, resultat);
+        }
+
+        [TestMethod]
+        public void TestNbPointsMot()
+        {
+            Jeu jeu = new Jeu();
+            int resultat = jeu.NbPointsMot("radis");
+
+            Assert.AreEqual(18, resultat);
+        }
+
+    }
+}
